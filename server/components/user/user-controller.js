757877244a8ea2c56d02af689e266e56c06025c8
Teody C. Seguin <teodyseguin@gmail.com>
2016-08-25 01:35:37 +0800

    refactor(user controller): make an update to user controller
    + Add the user model
    + Add the logic saving a document. This is a temporary solution
    for the mean time

--- server/components/user/user-controller.js
+++ server/components/user/user-controller.js
@@ -1,12 +1,22 @@
 'use strict';
 
+const UserModel = require('./user-model').UserModel;
+
 class UserController {
-    constructor() {
-        super();
-    }
-
     create(req, res, next) {
+        console.log('> create()');
 
+        var userModel = new UserModel();
+        var model = userModel.getUserModel();
+
+        if (model) {
+            var user = new model({email: 'someone@email.com', name: 'teody', password: 'gunraixfillup@#1234'});
+            user.save(err => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        }
     }
 }
 
