c4896969717328b225045035375662d3fca35468
Teody C. Seguin <teodyseguin@gmail.com>
2016-08-25 01:32:51 +0800

    refactor(server): update server script
    + Added a service for establishing database connection
    + Added a self executing function which contains the
    logic for getting the connection, establishing the connection
    and then, running the app server

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@
 
 const express = require('express'),
     userRouter = require('./components/user/user-router'),
-    dbService = require('./services/dbconnection').DBConnection;
+    dbService = require('./services/dbconnection');
 
 let app = express();
 
-app.use('/v1/wallet/api', function() {});
+// app.use('/v1/wallet/api', function() {});
 
 // API to create a user
 app.use('/v1/wallet/api/users', userRouter);
@@ -20,20 +20,24 @@ app.put('/v1/wallet/api/users/<id>/credit/<amount>', function() {});
 // API to pull a specific user account balance
 app.get('/v1/wallet/api/users/<id>/balance', function() {});
 
-dbService.connect(err => {
-    if (err) {
-        console.log(err);
-        console.log('can\'t connect to MLab')
-        return;
+(function() {
+    if (!dbService.getConnection()) {
+        dbService.connect(err => {
+            if (err) {
+                console.log(err);
+                console.log('can\'t connect to MLab')
+                return;
+            }
+
+            console.log('> Connected to MLab');
+            console.log('> Initiating the App');
+
+            let server = app.listen(3000, function () {
+                let host = server.address().address;
+                let port = server.address().port;
+
+                console.log('> Wallet API Started!', host == '::' ? 'localhost': host, port);
+            });
+        });
     }
-
-    console.log('> Connected to MLab');
-    console.log('> Initiating the App');
-
-    let server = app.listen(3000, function () {
-        let host = server.address().address;
-        let port = server.address().port;
-
-        console.log('Wallet API Started!', host, port);
-    });
-});
+})();
