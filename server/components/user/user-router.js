c3e0bd9ad918b0953688486e99daedba06ec489a
Teody C. Seguin <teodyseguin@gmail.com>
2016-08-25 11:48:06 +0800

    docs(user router): tell when the api will be called

--- server/components/user/user-router.js
+++ server/components/user/user-router.js
@@ -3,6 +3,7 @@
 const router = require('express').Router(),
     controller = require('./user-controller').UserController;
 
+// When post /v1/wallet/api/users is called
 router.post('/', controller.create);
 
 module.exports = router;
