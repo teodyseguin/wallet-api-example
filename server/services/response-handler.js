8ae4e9c7707377e1dfb2cd468cb848f681c4ddb8
Teody C. Seguin <teodyseguin@gmail.com>
2016-08-26 01:39:41 +0800

    refactor(print response): remove else guard

--- server/services/response-handler.js
+++ server/services/response-handler.js
@@ -16,18 +16,23 @@ function printResponse(error, response, docs) {
     if (error) {
         response.write(JSON.stringify(error));
         response.end();
-        return console.log(error);
+        return {
+            error: true,
+            message: error
+        };
     }
-    else {
-        try {
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            response.write(JSON.stringify(docs));
-            response.end();
+
+    try {
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.write(JSON.stringify(docs));
+        response.end();
+        return {
+            error: false
         }
-        catch(err) {
-            console.log('Response Header is only written once...');
-            response.end();
-        }
+    }
+    catch(err) {
+        console.log('Response Header is only written once...');
+        response.end();
     }
 }
 
