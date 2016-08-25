904b22d62f55b94518f4ea878fdbf79561eaf76d
Teody C. Seguin <teodyseguin@gmail.com>
2016-08-25 11:22:36 +0800

    refactor(user controller): modify create() logic
    + Make use of promise thenable from user.save()
    + Remove next argument from create. I have no use for it
    for the meantime

--- server/components/user/user-controller.js
+++ server/components/user/user-controller.js
@@ -4,15 +4,22 @@ const responseHandler = require('../../services/response-handler'),
     UserModel = require('./user-model').UserModel;
 
 class UserController {
-    create(req, res, next) {
-        var userModel = new UserModel();
-        var model = userModel.getUserModel();
+    create(req, res) {
+        let userModel = new UserModel(),
+            model = userModel.getUserModel();
 
         if (model) {
-            var user = new model(req.body);
-            user.save(err => {
-                responseHandler.printResponse(err, res, {});
-            });
+            let user = new model(req.body);
+            user
+                .save()
+                .then(
+                    function userResolution(u) {
+                        responseHandler.printResponse(null, res, u);
+                    },
+                    function userRejection(err) {
+                        responseHandler.printResponse(err, res, null);
+                    }
+                );
         }
     }
 }
