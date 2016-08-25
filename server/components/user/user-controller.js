4713cc70c5b89471d4aa542e2101717797676bfc
Teody C. Seguin <teodyseguin@gmail.com>
2016-08-25 11:50:33 +0800

    docs(user controller): add documentation to class method create()

--- server/components/user/user-controller.js
+++ server/components/user/user-controller.js
@@ -4,10 +4,20 @@ const responseHandler = require('../../services/response-handler'),
     UserModel = require('./user-model').UserModel;
 
 class UserController {
+    /**
+     * Method to create a user account, and store it to the database
+     *
+     * @param req
+     *  The request parameter
+     * @param res
+     *  The response parameter
+     */
     create(req, res) {
         let userModel = new UserModel(),
             model = userModel.getUserModel();
 
+        // I just want to make sure that a model is generated
+        // and I don't want to continue when it is none
         if (model) {
             let user = new model(req.body);
             user
