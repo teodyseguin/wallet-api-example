28531e9fac3656608472545cc28bff84b77989f0
Teody C. Seguin <teodyseguin@gmail.com>
2016-08-26 12:39:09 +0800

    refactor(logger): add a return statement to logger service

--- server/services/logger.js
+++ server/services/logger.js
@@ -3,7 +3,10 @@
 function logger(string) {
     if (process.env.NODE_ENV !== 'test') {
         console.log('logger:' + string);
+        return;
     }
+
+    return false;
 }
 
 module.exports.logger = logger;
