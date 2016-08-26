b064d7d593158dd28758aca1f40bfb75e5cfeca7
Teody C. Seguin <teodyseguin@gmail.com>
2016-08-26 12:39:54 +0800

    test(logger test): add a unit test for logger service

---
+++ test/services/logger.spec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const logger = require('../../server/services/logger').logger,
+    should = require('should'),
+    sinon = require('sinon');
+
+describe('Testing Logger service method', function() {
+    it('logger() should print nothing and return false if environment is set to test', function() {
+        let loggerMessage = 'logger text!',
+            result = logger(loggerMessage);
+
+        // On previous test files, the environment was already set to
+        // 'test' and that has been inherited here in some way. Therefore
+        // we can surely expect that logger() will return false and will
+        // not print anything on the screen
+        result.should.equal(false);
+    });
+});
