538a15a5cd4f6b9b1fbf63eb2db8c4be9d5f4646
Teody C. Seguin <teodyseguin@gmail.com>
2016-08-26 20:16:41 +0800

    refactor(user controller): add class constructor
    We need a class constructor to initialise the UserModel object
    which has a refence to the created 'user model'

--- server/components/user/user-controller.js
+++ server/components/user/user-controller.js
@@ -4,6 +4,14 @@ const responseHandler = require('../../services/response-handler'),
     UserModel = require('./user-model').UserModel;
 
 class UserController {
+    constructor() {
+        // Initially set this to null, but on succeeding calls, we need
+        // to cache the value from here. The reason is to prevent
+        // from getting the error of overwriting the model, which is
+        // already been compiled by mongoose
+        this.Model = null;
+    }
+
     /**
      * Method to create a user account, and store it to the database
      *
@@ -13,13 +21,16 @@ class UserController {
      *  The response parameter
      */
     create(req, res) {
-        let userModel = new UserModel(),
-            model = userModel.getUserModel();
+        if (this.Model == null) {
+            this.Model = new UserModel();
+        }
+
+        let modelInstance = this.Model.getUserModel();
 
         // I just want to make sure that a model is generated
         // and I don't want to continue when it is none
-        if (model) {
-            let user = new model(req.body);
+        if (modelInstance) {
+            let user = new modelInstance(req.body);
             user
                 .save()
                 .then(
