2d6ade98b3de96016df951b108d3dc5fd1fe98d1
Teody C. Seguin <teodyseguin@gmail.com>
2016-08-28 18:42:14 +0800

    refactor(user test): refactory user model test
    This is to accommodate the changes I've made on the user model

--- test/components/user/user-model.spec.js
+++ test/components/user/user-model.spec.js
@@ -6,7 +6,7 @@ const assert = require('assert'),
     dataconf = require('../../../server/config')['test'],
     dbService = require('../../../server/services/dbconnection'),
     should = require('should'),
-    UserModel = require('../../../server/components/user/user-model').UserModel;
+    User = require('../../../server/components/user/user-model').User;
 
 describe('Testing User Component', function() {
     let connectionOptions = {
@@ -16,33 +16,33 @@ describe('Testing User Component', function() {
             user: dataconf.mongodb.username,
             pass: dataconf.mongodb.password,
             promiseLibrary: require('bluebird')
-        };
+        },
+        UserObject = new User();
+
+    it('User should have a method getUserModel()', function() {
+        should.exists(UserObject.getUserModel);
+    });
+
+    it('User should have a method getUserSchema()', function() {
+        should.exists(UserObject.getUserSchema);
+    });
 
     dbService.connect(
         dataconf.mongodb.host,
         dataconf.mongodb.dbname,
         dataconf.mongodb.port,
         connectionOptions,
-        err => {}
+        err => {
+            let model = UserObject.getUserModel(),
+                schema = UserObject.getUserSchema();
+
+            it('getUserSchema() should return the schema object', function() {
+                assert.equal('object', typeof schema);
+            });
+
+            it('getUserModel() should return the model function', function() {
+                assert.equal('function', typeof model);
+            });
+        }
     );
-
-    let userModel = new UserModel(),
-        model = userModel.getUserModel(),
-        schema = userModel.getUserSchema();
-
-    it('UserModel should have a method getUserModel()', function() {
-        should.exists(userModel.getUserModel);
-    });
-
-    it('UserModel should have a method getUserSchema()', function() {
-        should.exists(userModel.getUserSchema);
-    });
-
-    it('getUserSchema() should return the schema object', function() {
-        assert.equal('object', typeof schema);
-    });
-
-    it('getUserModel() should return the model function', function() {
-        assert.equal('function', typeof model);
-    });
 });
