df60aa59f94bd4e511afedf711585b4889581c42
Teody C. Seguin <teodyseguin@gmail.com>
2016-08-30 23:25:48 +0800

    feat(credit controller): initial credit controller
    This will change later on as the development goes in progress

--- server/components/credit/credit-controller.js
+++ server/components/credit/credit-controller.js
@@ -1,8 +1,49 @@
 'use strict';
 
-class CreditController {
-    create() {
+const paypal = require('paypal-rest-sdk'),
+    env = process.env.NODE_ENV || 'dev',
+    dataconf = require('../../config')[env];
 
+class CreditController {
+    credit(req, res) {
+        let paymentObj = {
+            intent: 'sale',
+            payer: {
+                payment_method: 'paypal'
+            },
+            redirect_urls: {
+                return_url: 'http://127.0.0.1:3000/execute',
+                cancel_url: 'http://127.0.0.1:3000/cancel'
+            },
+            transactions: [{
+                amount: {
+                    total: 3,
+                    currency: 'USD'
+                },
+                description: 'payment'
+            }]
+        };
+
+        // load the paypal configuration and make use of it
+        // from the succeeding procedures below
+        paypal.configure(dataconf.paypal_sdk.api);
+
+        paypal.payment.create(paymentObj, function(error, payment) {
+            if (error) {
+                console.log(error);
+            }
+            else {
+                if (payment.payer.payment_method === 'paypal') {
+                    let payerId;
+
+                    for (var index = 0; index < payment.links.length; index++) {
+                        if (payment.links[index].rel === 'approval_url') {
+
+                        }
+                    }
+                }
+            }
+        });
     }
 }
 
