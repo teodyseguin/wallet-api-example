b948f96c57fafa7588bc051ca86aa7ee94648db0
Teody C. Seguin <teodyseguin@gmail.com>
2016-08-31 18:34:13 +0800

    refactor(load controller): remove unnecessary dependency

--- server/pages/load/load-controller.js
+++ server/pages/load/load-controller.js
@@ -1,13 +1,9 @@
 'use strict';
 
-const paypal = require('paypal-rest-sdk'),
-    env = process.env.NODE_ENV || 'dev',
-    dataconf = require('../../config')[env],
-    path = require('path');
+const path = require('path');
 
 class LoadController {
     page(req, res) {
-        // console.log(path.join(__dirname + '/pages/load-wallet.html'));
         res.sendFile(path.join(__dirname + '/load.html'));
     }
 }
