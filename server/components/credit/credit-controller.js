a4a0bceca1fb085459a89dda9c337c5bec1bc668
Teody C. Seguin <teodyseguin@gmail.com>
2016-09-01 02:20:10 +0800

    refactor(credit controller): layout the logic for submitting a paypal payment

--- server/components/credit/credit-controller.js
+++ server/components/credit/credit-controller.js
@@ -1,46 +1,36 @@
 'use strict';
 
-const paypal = require('paypal-rest-sdk'),
-    env = process.env.NODE_ENV || 'dev',
-    dataconf = require('../../config')[env];
+const env = process.env.NODE_ENV || 'dev',
+    dataconf = require('../../config')[env],
+    paypal = require('paypal-rest-sdk'),
+    PaymentMethod = require('../../helpers/payment').PaymentMethod,
+    response = require('../../helpers/response-handler');
 
 class CreditController {
     credit(req, res) {
-        let paymentObj = {
-            intent: 'sale',
-            payer: {
-                payment_method: 'paypal'
-            },
-            redirect_urls: {
-                return_url: 'http://127.0.0.1:3000/execute',
-                cancel_url: 'http://127.0.0.1:3000/cancel'
-            },
-            transactions: [{
-                amount: {
-                    total: 3,
-                    currency: 'USD'
-                },
-                description: 'payment'
-            }]
-        };
+        let payment = new PaymentMethod(),
+            method = req.body.payment_method,
+            paymentObj = payment[method](req.body);
 
         // load the paypal configuration and make use of it
         // from the succeeding procedures below
         paypal.configure(dataconf.paypal_sdk.api);
 
-        paypal.payment.create(paymentObj, function(error, payment) {
+        paypal.payment.create(paymentObj, (error, payment) => {
             if (error) {
-                console.log(error);
+                response.printResponse(error, res, {});
             }
             else {
                 if (payment.payer.payment_method === 'paypal') {
-                    let payerId;
+                    let redirectUrl;
 
                     for (var index = 0; index < payment.links.length; index++) {
                         if (payment.links[index].rel === 'approval_url') {
-
+                            redirectUrl = payment.links[index].href;
                         }
                     }
+
+                    response.printResponse(null, res, { redirect: redirectUrl });
                 }
             }
         });
