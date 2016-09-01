a00be198f5a385b3849e70f8a1f2b09855a01d43
Teody C. Seguin <teodyseguin@gmail.com>
2016-09-02 00:44:11 +0800

    refactor(auth router): update route to authentication and get rid of the logout route

--- server/components/auth/auth-router.js
+++ server/components/auth/auth-router.js
@@ -3,17 +3,9 @@
 const router = require('express').Router(),
     passport = require('passport');
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
+router.post('/', passport.authenticate('local'), function(req, res) {
     res.json({
         message: 'User authenticated'
-    });
-});
-
-router.get('/logout', (req, res) => {
-    req.logout();
-    res.status(200).json({
-        status: 'ok',
-        message: 'Logged out'
     });
 });
 
