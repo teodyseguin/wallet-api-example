5aa8c785ad3f528267d37a7cae2909349ba65f53
Teody C. Seguin <teodyseguin@gmail.com>
2016-09-02 11:56:23 +0800

    feat(credit router): add get route for retrieve

--- server/components/credit/credit-router.js
+++ server/components/credit/credit-router.js
@@ -4,6 +4,7 @@ const router = require('express').Router(),
     authService = require('../auth/auth-service'),
     controller = require('./credit-controller').CreditController;
 
+router.get('/', authService.ensureAuthenticated, controller.retrieve);
 router.post('/', authService.ensureAuthenticated, controller.credit);
 
 module.exports = router;
